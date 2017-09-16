70ad938dc740109fc00ef9cfc4f8b65893209acc
Kostas Konstandoulakis <kostas@webtrails.gr>
2017-09-16 21:47:35 +0300

    fetch data from db to show logs

--- memberpage.php
+++ memberpage.php
@@ -9,12 +9,17 @@ $title = 'Members Page';
 //include header template
 require('layout/header.php');
 require('layout/nav.php'); 
+$user_session_id=$_SESSION['memberID'];	
+		
+$smtp = $db->prepare("SELECT * FROM `members` WHERE `memberID`=$user_session_id");						
+$smtp->execute();
+$row1 = $smtp->fetch();
+$is_admin=$row1['is_admin'];
+$show_admin = ($is_admin==1) ? '':'`user_id`=' . $user_session_id . ' AND'; 
 ?>
 
 <div class="container">
-
 	<div class="row">
-
 	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3 text-center">			
 			<h2>Member page - Welcome <?php echo $_SESSION['username']; ?></h2>
 		</div>
@@ -28,75 +33,101 @@ require('layout/nav.php');
 			<div class="tab-content">
 				<div id="game1" class="tab-pane fade in active">
 					<h3 class="text-center">Game 1 Logs</h3>
-					<div class="table-responsive">
-						<table class="table table-bordered table-hover">
-						<caption class="text-center">Logs History</caption>
-						<thead>
-							<tr>
-							<th>Date</th>
-							<th>Level</th>
-							<th>Category</th>
-							<th>Clicks</th>
-							<th>Moves</th>
-							<th>Time (sec)</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-							<td>19/05/2017</td>
-							<td>1</td>
-							<td>Fruits</td>
-							<td>tile1 (Orange): 2 Clicks  tile2 (Orange): 2 Clicks  tile3 (Melon): 1 Clicks  tile4 (Apricot): 1 Clicks  tile5 (Pear): 2 Clicks  tile6 (Melon): 1 Clicks  tile7 (Pear): 2 Clicks  tile8 (Apricot): 1 Clicks </td>
-							<td>12</td>
-							<td>12</td>
-							</tr>
-							<tr>
-							<td>19/05/2017</td>
-							<td>2</td>
-							<td>Colors</td>
-							<td>37.3</td>
-							<td>12</td>
-							<td>50</td>
-							</tr>
-							<tr>
-							<td>19/05/2017</td>
-							<td>3</td>
-							<td>Fruits</td>
-							<td>43.2</td>
-							<td>12</td>
-							<td>70</td>
-							</tr>
-							<tr>
-							<td>19/05/2017</td>
-							<td>1</td>
-							<td>Colors</td>
-							<td>39.1</td>
-							<td>12</td>
-							<td>30</td>
-							</tr>
-							<tr>
-							<td>19/05/2017</td>
-							<td>2</td>
-							<td>Faces</td>
-							<td>38.4</td>
-							<td>12</td>
-							<td>40</td>
-							</tr>
-							<tr>
-							<td>19/05/2017</td>
-							<td>3</td>
-							<td>Fruits</td>
-							<td>41.1</td>
-							<td>12</td>
-							<td>55</td>
-							</tr>
-						</tbody>								
+					<div class="table-responsive">						
+						<?php								
+						$result = $db->prepare("SELECT `game_sessions`.*, `artifact_types`.`art_type`,`levels`.`difficulty_characterization`
+												FROM `game_sessions` 
+												INNER JOIN `artifact_types` 
+												ON `game_sessions`.`game_category` =  `artifact_types`.`art_id` 
+												INNER JOIN `levels` 
+												ON `game_sessions`.`game_level` =  `levels`.`id`
+												WHERE $show_admin `game_sessions`.`game_id`=1");
+												
+						$result->execute();
+						$number_of_rows = $result->rowCount();
+						if($number_of_rows>0  ){
+						?>
+						<table class="table table-bordered table-hover normaltopmargin">
+							<caption class="text-center">Logs History</caption>
+							<thead>
+								<tr>
+								<th>Date</th>
+								<th>Level</th>
+								<th>Category</th>
+								<th>Clicks</th>
+								<th>Moves</th>
+								<th>Time (sec)</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php 
+								for($i=0; $row = $result->fetch(); $i++){ 
+								$_SESSION['is_admin'] = $row['is_admin'];
+							?>
+								<tr>
+								<td><?php echo $row['start_datetime']; ?></td>
+								<td><?php echo $row['difficulty_characterization']; ?></td>
+								<td><?php echo $row['art_type']; ?></td>
+								<td><?php echo $row['game_clicks']; ?></td>
+								<td><?php echo $row['game_moves']; ?></td>
+								<td><?php echo $row['game_time_finished']; ?></td>
+								</tr>
+								<?php } ?>
+							<?php }else{ ?>
+							<p class="text-center">Logs Not found</p>
+							<?php } ?>
+							</tbody>								
 						</table>
 					</div><!--end of .table-responsive-->
 				</div>
 				<div id="game2" class="tab-pane fade">
-				<h3>Game 2 Logs</h3>
-				<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<h3 class="text-center">Game 2 Logs</h3>
+					<div class="table-responsive">						
+						<?php 		
+						$result = $db->prepare("SELECT `game_sessions`.*, `artifact_types`.`art_type`,`levels`.`difficulty_characterization`
+												FROM `game_sessions` 
+												INNER JOIN `artifact_types` 
+												ON `game_sessions`.`game_category` =  `artifact_types`.`art_id` 
+												INNER JOIN `levels` 
+												ON `game_sessions`.`game_level` =  `levels`.`id`
+												WHERE $show_admin `game_sessions`.`game_id`=2");
+												
+						$result->execute();
+						$number_of_rows = $result->rowCount();
+						if($number_of_rows>0  ){
+						?>
+						<table class="table table-bordered table-hover normaltopmargin">
+							<caption class="text-center">Logs History</caption>
+							<thead>
+								<tr>
+								<th>Date</th>
+								<th>Level</th>
+								<th>Category</th>
+								<th>Clicks</th>
+								<th>Moves</th>
+								<th>Time (sec)</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php 
+								for($i=0; $row = $result->fetch(); $i++){ 
+								$_SESSION['is_admin'] = $row['is_admin'];
+							?>
+								<tr>
+								<td><?php echo $row['start_datetime']; ?></td>
+								<td><?php echo $row['difficulty_characterization']; ?></td>
+								<td><?php echo $row['art_type']; ?></td>
+								<td><?php echo $row['game_clicks']; ?></td>
+								<td><?php echo $row['game_moves']; ?></td>
+								<td><?php echo $row['game_time_finished']; ?></td>
+								</tr>
+								<?php } ?>
+							<?php }else{ ?>
+							<p class="text-center">Logs Not found</p>
+							<?php } ?>
+							</tbody>								
+						</table>
+					</div><!--end of .table-responsive-->
 				</div>		
 			</div>
 		</div>		
