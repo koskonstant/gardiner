8903a29e894028fa691c74b32a1122485341e98a
Kostas Konstandoulakis <kostas@webtrails.gr>
2017-09-22 20:04:05 +0300

    update script and css paths fix bug

--- layout/footer.php
+++ layout/footer.php
@@ -3,10 +3,10 @@
 		<div class="pull-right">ICT 2017</div>
 	</div>
 </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.3/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-json/2.6.0/jquery.json.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/t/bs-3.3.6/dt-1.10.11/datatables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/materialize.js"></script>
 <script src="js/custom.js"></script>
